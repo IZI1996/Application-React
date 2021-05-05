7c37541b7c6c3b2b79cefaa2755b847dc5eb2a12
kaoutar izi <izkawthar@gmail.com>
2021-05-05 09:41:25 +0000

    react-js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router ,Switch ,Route } from 'react-router-dom';
+import Search from './components/Search';
+import Home from './components/Home';
+import Details from './components/Details';
+import Favorite from './components/Favorite';
+import {React,useEffect} from 'react'
 
 function App() {
+  useEffect(() => {
+    
+    localStorage.setItem("data",JSON.stringify([]));
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+       <div className="App">
+         <Switch>
+            <Route path="/search/:name" component={Search}/>
+            <Route path="/details/:id" component={Details}/>
+            <Route path="/favorite" component={Favorite}/>
+            <Route path="/" component={Home} exact/>
+         </Switch>
+        </div>
+    </Router>
   );
 }
 
